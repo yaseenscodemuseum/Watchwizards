513d2adc76ebbe0a947343be85bfc8b14beec39f
Yaseen <yaseenabdulaziz18@gmail.com>
2025-06-02 23:09:51 +0530

    railway

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     domains: ['image.tmdb.org'], // For movie posters
   },
+  distDir: '.next',
+  experimental: {
+    appDir: true
+  }
 }
 
 export default nextConfig; 
